b48a3009088c2d82c5558b38fd5de3e3f34660df
Bui Anh Tuan <buianhtuan2903@gmail.com>
2019-09-06 22:38:19 +0700

    .

--- src/app/@pages/allproduct/allproduct.component.ts
+++ src/app/@pages/allproduct/allproduct.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from 'src/app/@services/database.service';
+import { Observable } from 'rxjs';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Products } from 'src/app/@services/productdata';
+import { AuthService } from 'src/app/@services/auth.service';
+import { StorageService } from 'src/app/@services/storage.service';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { finalize } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-allproduct',
@@ -7,15 +17,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllproductComponent implements OnInit {
 
-  constructor() { }
+  allproduct: AngularFirestoreCollection<Products>;
+  products: Observable<Products[]>;
+  public lists1: Observable<Products[]>;
+
+  constructor(private database: AngularFirestore) { }
 
   ngOnInit() {
+    this.allproduct = this.database.collection('products');
+    this.products = this.allproduct.valueChanges();
+    this.createlist1();
   }
-  lists1 =[
-    {name: 'Mau 1'},
-    {name: 'Mau 2'},
-    {name: 'Mau 3'},
-  ];
+
+  createlist1(){ 
+    this.database.collection("products").ref.where("type", "==", "Màu vẽ")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+  }
+  // lists1 =[
+  //   {name: 'Mau 1'},
+  //   {name: 'Mau 2'},
+  //   {name: 'Mau 3'},
+  // ];
 
   lists2 =[
     {name: 'Mau 1'},
