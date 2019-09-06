efe2b45b5dc63f639a729b8677c4f4a7c73e142f
anhtuan2903 <satf2903@gmail.com>
2019-09-06 16:57:24 +0700

    zz

--- src/app/@services/productdata.ts
+++ src/app/@services/productdata.ts
@@ -1,8 +1,17 @@
-
 export class Products{
     id: string;
     name: string;
     type: string;
     price: number;
     imgURL: string;
+}
+
+export class Orders{
+    id: string;
+    name: string;
+    price: number;
+    product: string;
+    quantity: number;
+    time: string;
+    phonenumber: number;
 }
